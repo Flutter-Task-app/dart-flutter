0f019629c82981c45a979e234b4d0829834b39ad
nandhakumar <nandhu@m2p.in>
2023-08-18 10:25:05 +0530

    Initial commit dart

---
+++ bin/dart_flutter.dart
@@ -0,0 +1,5 @@
+import 'package:dart_flutter/dart_flutter.dart' as dart_flutter;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_flutter.calculate()}!');
+}
---
+++ test/dart_flutter_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_flutter/dart_flutter.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
